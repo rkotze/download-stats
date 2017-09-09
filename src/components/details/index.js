9be24174ae18d403a290d220d87f98f3b9bc1c75
Richard Kotze <rkotze@findmypast.com>
2017-09-09 12:39:52 +0100

    Add icons to details page for date range and downloads.

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -9,7 +9,7 @@ import 'c3/c3.min.css';
 function Details({params}) {
   return (
     <div>
-    	<h2>{capitalize(params.name)} for the last 30 days</h2>
+    	<h2><i className="fa fa-bar-chart fa-3x" aria-hidden="true"></i> {capitalize(params.name)} for the last 30 days</h2>
     	<div><OneMonthPackage packageName={params.name} /></div>
     </div>
   );
@@ -31,12 +31,12 @@ function PackageDownloadList({packageName, success, failure}){
 	if(success && failure === null){
 		const { downloads, start, end } = success;
 		return (<div>
-				<p>
+				<p style={s.p}><i className="fa fa-calendar fa-2x" aria-hidden="true"></i>&nbsp;
 					<em>{moment(start).format('Do MMM YYYY')}</em> -&nbsp;
 					<em>{moment(end).format('Do MMM YYYY')}</em>
 				</p>
-				<p style={s.p}>
-					Total: <strong>
+				<p style={s.p} className="fa-2x">
+					<strong><i className="fa fa-arrow-circle-o-down" aria-hidden="true"></i>&nbsp;
 						{downloads.reduce(function(total, downloads, i){
 							return total + downloads.downloads;
 						}, 0)}
